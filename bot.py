59bb66059de2ca1f57b925442dc66b5f8c8b137d
Tiffany Lin <tiffany.l.lin@student.unsw.edu.au>
2025-09-08 20:01:22 +1000

    implement Discord bot with hello command

--- bot.py
+++ bot.py
@@ -1,1 +1,29 @@
 # A program that allows bot to respond to my messages in a discord channel 
+
+# Import and install required packages
+import discord as dc
+
+class MyClient(dc.Client):
+    # Event handler: Called when the bot successfully logs in
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+    
+    """
+    Event handler: Listens for specific commands in messages and responds accordingly.
+    Ignores messages sent by the bot itself to avoid loops.
+    """
+    async def on_message(self, message):
+        # Ignores messages sent by bot itself
+        if message.author == self.user:
+            return
+        # Responds to $hello command
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+# Allow bot to read message content
+intents = dc.Intents.default()
+intents.message_content = True
+
+# Initialises the client using own specific bot's token
+client = MyClient(intents=intents)
+client.run('YOUR_DISCORD_BOT_TOKEN') # replace with your own discord bot token 
